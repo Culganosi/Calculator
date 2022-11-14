aba2f462d61da24a0b775ed8f18955f62d310a1e
Adam <72367647+Culganosi@users.noreply.github.com>
2022-11-14 16:36:56 -0500

    change namespace

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,11 +6,12 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Currency : {0:c}", 23.455);
             Console.WriteLine("Pad with 0s : {0:d4}", 23);
             Console.WriteLine("3 Decimals : {0:f3}", 23.45555);
             Console.WriteLine("Commas : {0:n4}", 2300);
-            // single-line comments same
+            // single-line comments same (This is NOT a Calculator!)
             /* multi line comments are the
              * same 
              */
