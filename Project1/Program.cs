7a5cf9a54cf24e974e74fc56adb913899dbe61a2
Adam <72367647+Culganosi@users.noreply.github.com>
2022-11-14 20:00:09 -0500

    playing around

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -6,7 +6,18 @@ namespace Project1
     {
         static void Main(string[] args)
         {
+            string appName = "Guessing Game";
+            string appVersion = "1.0.0";
+            string appAuthor = "Culgan";
 
+            // Change Console Text Color
+            Console.ForegroundColor= ConsoleColor.Green;
+            
+            // Write App info
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            // Change Console Text Color back to White
+            Console.ResetColor();
         }
     }
 }
